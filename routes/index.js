970cc77dc01a353bd079fc411210412f1456433c
adarshtfs <adarsh.raj@taxiforsure.com>
2015-07-10 23:52:11 +0530

    modified some apis

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ var admin=require('./admin.js');
 var vendor=require('./vendor.js');
 
 router.get('/admin/',admin.check);
-router.post('/admin//login',admin.login);
+router.post('/admin/login',admin.login);
 router.post('/admin/new',admin.createAdmin);
 
 /* all vendor related routes */
 
 router.get('/vendor/login',vendor.login);
 router.post('/vendor/new',vendor.createVendor);
-router.post('vendor/listing/new',vendor.createListing);
+router.post('/vendor/listing/new',vendor.createListing);
 router.get('/vendor/new-requests',vendor.getNewrequests);
 router.get('/vendor/replied-requests',vendor.repliedRequests);
 router.get('/vendor/confimed-bookings',vendor.confirmedBookings);
